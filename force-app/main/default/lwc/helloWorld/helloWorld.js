6bb55731715adbbfe8602227a26bf019bb6d2bbf
Sardor Isakov <sisardor@gmail.com>
2019-02-07 09:24:44 -0800

    major updates

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -3,6 +3,7 @@ import NAME_Fields from '@salesforce/schema/Account.Name';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
 import resourceUrl from '@salesforce/resourceUrl/lib';
+// import resourceUrl from '@salesforce/resourceUrl/lib';
 
 export default class HelloWorld extends LightningElement {
   divHeight = 600;
@@ -14,7 +15,8 @@ export default class HelloWorld extends LightningElement {
     this.uiInitialized = true;
 
     Promise.all([
-        loadScript(this, resourceUrl + '/webviewer.min.js')
+        loadScript(this, resourceUrl + '/webviewer.min.js'),
+        loadScript(this, resourceUrl + '/../myfiles/license-keyz.js')
     ])
     .then(() => {
        
@@ -45,20 +47,21 @@ export default class HelloWorld extends LightningElement {
     console.log()
     console.log(PDFTron, this)
     let me = this;
-    var url = 'https://energy-dream-2305-dev-ed.lightning.force.com/resource/1549157318000/myfiles/compressed.tracemonkey-pldi-09.pdf';
+    var url = resourceUrl + '/../myfiles/webviewer-demo-annotated.pdf';
     // const viewer = this.template.querySelector('div.pdf_viewer');
     let viewerElement = this.template.querySelector('div')//('div.pdf_viewer');
     let myWebViewer = new PDFTron.WebViewer({
       path: resourceUrl, // path to the PDFTron 'lib' folder on your server
-      l: 'demo:sisakov@pdftron.com:750d07bd01a53f57075ae2b0404e99b52d2335d64b4a5e4767',
+      l: window.sampleL,
+      config: '',
+      // fullAPI: true,
       initialDoc: url,
-      // initialDoc: '/path/to/my/file.pdf',  // You can also use documents on your server
     }, viewerElement);
     
     viewerElement.addEventListener('ready', function() {
-      let CoreControls = viewerElement.querySelector('iframe').contentWindow.CoreControls;
-      console.log(CoreControls)
-      console.log(CoreControls.setPDFWorkerPath);
+      // let CoreControls = viewerElement.querySelector('iframe').contentWindow.CoreControls;
+      // console.log(CoreControls)
+      // console.log(CoreControls.setPDFWorkerPath);
       
       
     })
---
+++ force-app/main/default/staticresources/lib/core/Override.js
@@ -0,0 +1,7 @@
+(function(exports) {
+console.log('override')
+// exports.CoreControls.setPDFWorkerPath('worker/lean')
+// exports.CoreControls.setPDFResourcePath('../../../')
+// exports.CoreControls.setPDFAsmPath('asm/lib/core/pdf')
+
+})(typeof window === 'undefined' ? this : window);
